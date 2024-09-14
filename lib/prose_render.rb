5ba474ae269890cb8a58995a0b9f4935fd0c3935
Matt Barry <mbarry@gmail.com>
2024-09-14 12:23:26 -0400

    initial commit

---
+++ lib/prose_render.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "prose_render/version"
+
+module ProseRender
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/prose_render/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ProseRender
+  VERSION = "0.1.0"
+end
